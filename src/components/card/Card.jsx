6f3f27bc06f1580ea5794829aab0cef4b00a8bea
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2024-11-10 16:25:45 +0100

    added description

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,8 +5,13 @@ export default function Card({ name, description, image }) {
     <div id={name} className="card__body">
       <img src={image} alt="project" />
       <div>
-        <h3>{name}</h3>
-        <p>{description}</p>
+        <h2>{name}</h2>
+        {description.map((item, index) => (
+          <div key={index}>
+            <h4>{item.header}</h4>
+            <p>{item.text}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
